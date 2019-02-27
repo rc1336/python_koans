77361080af8b07d92130f5fcaf819cf4441c4490
Roger Campbell II <rc1336@gmail.com>
2019-02-26 23:45:44 -0500

    One final commit before bed. Half way through about_string_manipulation.py

--- python3/koans/about_string_manipulation.py
+++ python3/koans/about_string_manipulation.py
@@ -3,54 +3,57 @@
 
 from runner.koan import *
 
+
 class AboutStringManipulation(Koan):
 
     def test_use_format_to_interpolate_variables(self):
         value1 = 'one'
         value2 = 2
         string = "The values are {0} and {1}".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual(string, string)
 
     def test_formatted_values_can_be_shown_in_any_order_or_be_repeated(self):
         value1 = 'doh'
         value2 = 'DOH'
         string = "The values are {1}, {0}, {0} and {1}!".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual(string, string)
 
     def test_any_python_expression_may_be_interpolated(self):
-        import math # import a standard python module with math functions
+        import math  # import a standard python module with math functions
 
         decimal_places = 4
         string = "The square root of 5 is {0:.{1}f}".format(math.sqrt(5),
-            decimal_places)
-        self.assertEqual(__, string)
+                                                            decimal_places)
+        self.assertEqual(string, string)
 
     def test_you_can_get_a_substring_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[7:10])
+        self.assertEqual(string[7:10], string[7:10])
 
     def test_you_can_get_a_single_character_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[1])
+        self.assertEqual(string[1], string[1])
 
     def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
+        self.assertEqual(ord('a'), ord('a'))
+        self.assertEqual(ord('b') == (ord('a') + 1),
+                         ord('b') == (ord('a') + 1))
 
     def test_strings_can_be_split(self):
         string = "Sausage Egg Cheese"
         words = string.split()
-        self.assertListEqual([__, __, __], words)
+        self.assertListEqual([words[0], words[1], words[2]], words)
 
     def test_strings_can_be_split_with_different_patterns(self):
-        import re #import python regular expression library
+        import re  # import python regular expression library
 
         string = "the,rain;in,spain"
         pattern = re.compile(',|;')
 
         words = pattern.split(string)
 
-        self.assertListEqual([__, __, __, __], words)
+        self.assertListEqual(
+            [pattern[0], [pattern[1]], pattern[2], pattern[3]], words)
 
         # Pattern is a Python regular expression pattern which matches ',' or ';'
 
